076c5d92da58561cccdb5dc663dd317c2f85dd25
RainerMorales <jerenmae212212@gmail.com>
2025-04-11 22:28:07 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { ModeToggle } from './components/mode-toggle';
 import Content from './Content'
-
+import Footer from './Footer'
+import { ThemeProvider } from "@/components/theme-provider";
 function App() {
 
 
   return (
     <>
+      <div className='flex justify-end'>
+        <ThemeProvider  defaultTheme="dark" storageKey="vite-ui-theme">
+          <ModeToggle></ModeToggle>
+        </ThemeProvider>
+      </div>
       <Content></Content>
+      <Footer></Footer>
     </>
   );
 }
--- src/Content.tsx
+++ src/Content.tsx
@@ -7,10 +7,12 @@ import { IoBookSharp } from "react-icons/io5";
 import { Badge } from "@/components/ui/badge";
 
 function Content() {
+  const summary =
+    "I'm a front-end developer passionate about creating interactive and visually engaging web experiences. I specialize in React, Vite, Tailwind CSS, and Framer Motion, bringing modern designs to life with smooth animations and responsive layouts. Always eager to learn and improve, I focus on building user-friendly interfaces that enhance the web experience";
   return (
     <>
-      <section className="mt-20  m-auto">
-        <div className="w-sm m-auto flex justify-center items-center gap-2">
+      <section className="mt-20 max-w-2xl m-auto">
+        <div className=" m-auto  gap-2  flex justify-center items-center">
           <div>
             <div className="font-bold text-3xl"> Hi! I'm Rainer</div>
             <div className="text-md "> Front-End Developer</div>
@@ -32,19 +34,15 @@ function Content() {
           </div>
         </div>
       </section>
-      <section className="mt-20 grid p-4 gap-6 max-w-2xl m-auto ">
-        <div className="space-y-4">
+      <section className="mt-20 p-4 grid gap-4 max-w-2xl m-auto ">
+        <div className="space-y-4 p-6 border rounded-md bg-accent">
           <h1 className="font-bold text-xl flex items-center gap-2">
             <IoBookSharp />
             Summary
           </h1>
-          <p className="opacity-80">
-            I aim to secure a position in the IT or management sectors, which
-            will allow me to utilize my education and skills to contribute to
-            the company's operations.
-          </p>
+          <p className="opacity-80">{summary}</p>
         </div>
-        <div className="space-y-4">
+        <div className="space-y-4 p-6 border rounded-md bg-accent">
           <h1 className="font-bold text-xl flex items-center gap-2">
             <BsMortarboardFill />
             Education
@@ -54,7 +52,7 @@ function Content() {
             <Badge className="bg-black text-white">2024</Badge>
           </div>
         </div>
-        <div className="space-y-4">
+        <div className="space-y-4 p-6 border rounded-md bg-accent">
           <h1 className="font-bold text-xl flex items-center gap-2">
             <FaLaptopCode />
             Tech Stack
@@ -82,14 +80,14 @@ function Content() {
             <span className="opacity-80 ">Developer Tools</span>
             <ul className="flex  flex-wrap gap-2 mt-1">
               {[
-                "HTML",
-                "CSS",
-                "Javascript",
-                "Tailwind CSS",
-                "Bootstrap",
-                "React js",
-                "Vite",
-                "Node js",
+                "Git",
+                "GitHub",
+                "VS Code",
+                "NPM",
+                "Vercel",
+                "Daisy UI",
+                "Framer Motion",
+                "React Bits",
               ].map((tool) => (
                 <li className="text-center" key={tool}>
                   <Badge className="bg-black text-white">{tool}</Badge>
